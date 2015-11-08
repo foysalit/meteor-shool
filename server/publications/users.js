2ba4ed3dc259a3ccb5efa20a0edbbb5fc36e4648
Foysal Ahamed <cfaion341@gmail.com>
2015-11-08 12:56:54 +0100

    polished up the datatable styling

--- server/publications/users.js
+++ server/publications/users.js
@@ -1,3 +1,36 @@
 Meteor.publish(null, function (){ 
 	return Meteor.roles.find({})
-})
+});
+
+Meteor.publishComposite("tabularUsersList", function (tableName, ids, fields) {
+	var table = Tables[tableName];
+	fields = _.extend(fields, table.extraFields);
+	check(tableName, String);
+	check(ids, Array);
+	check(fields, Match.Optional(Object));
+
+	this.unblock(); // requires meteorhacks:unblock package
+	var userId = this.userId;
+	// console.log(tableName, ids, fields);
+
+	return {
+		find: function () {
+			this.unblock(); // requires meteorhacks:unblock package
+
+			// check for admin role with alanning:roles package
+			if (!App.Users.isAdmin(userId)) {
+				return [];
+			}
+
+			var users = App.Users.collection.find({
+					_id: {$in: ids, $ne: userId}
+				}, {
+					fields: fields
+				});
+
+			console.log(users.count());
+			return users;
+		},
+		children: []
+	};
+});
