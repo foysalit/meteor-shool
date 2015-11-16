8718dad375179cee5c7d7f8543560481da8cb767
Foysal Ahamed <cfaion341@gmail.com>
2015-11-16 15:04:22 +0100

    added logo

--- mobile-config.js
+++ mobile-config.js
@@ -1,2 +1,17 @@
+App.icons({
+	// iOS
+	'iphone': 'resources/icons/Icon-60.png',
+	'iphone_2x': 'resources/icons/Icon-60@2x.png',
+	'iphone_3x': 'resources/icons/Icon-60@3x.png',
+	'ipad': 'resources/icons/Icon-76.png',
+	'ipad_2x': 'resources/icons/Icon-76@2x.png',
+
+	// Android
+	'android_ldpi': 'resources/icons/Icon-ldpi.png',
+	'android_mdpi': 'resources/icons/Icon-mdpi.png',
+	'android_hdpi': 'resources/icons/Icon-hdpi.png',
+	'android_xhdpi': 'resources/icons/Icon-xhdpi.png'
+});
+
 App.accessRule('*');
 App.accessRule('http://placeimg.com/*');
